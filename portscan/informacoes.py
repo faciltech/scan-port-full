8ac144ffd3557842b8a3f11ba118edbe8fd5fe7f
Eduardo <capamaral@gmail.com>
2026-02-16 17:21:05 -0300

    Refactor port selection and update banner display

--- portscan/informacoes.py
+++ portscan/informacoes.py
@@ -1,42 +1,68 @@
-#!/usr/bin/python
-print("    _________   ____________  ____________________  __");
-print("   / ____/   | / ____/  _/ / /_  __/ ____/ ____/ / / /");
-print("  / /_  / /| |/ /    / // /   / / / __/ / /   / /_/ / ");
-print(" / __/ / ___ / /____/ // /___/ / / /___/ /___/ __  /  ");
-print("/_/   /_/  |_\____/___/_____/_/ /_____/\____/_/ /_/   ");
-print("              ");
-print("Autor: Eduardo Amaral - eduardo4maral@protonmail.com")
-def escolher_listaPortas():
-	portas_top10 = [21,22,23,25,53,80,110,139,443,445,3389]
-	portas_top20 = [21,22,23,25,53,80,110,139,443,445,3389,135,139,143,443,445,993,995,1723,3306,3389,5900,8080]
-	portas_top50 = [21,22,23,25,53,80,110,139,443,445,3389,135,139,143,443,445,993,995,1723,3306,3389,5900,8080,3389,5060,5666,5900,6001,8000,8080,8443,8888,10000,32768,49152,49154]
-	portas_total = range(1,65535)
-	print("Selecione uma lista de Portas: ")
-	print("0 - Uma porta especifica")
-	print("1 - Lista top 10")
-	print("2 - Lista top 20")
-	print("3 - Lista top 50")
-	print("4 - Verificar as 65535 portas -- OBS: Levará muito tempo !!!")
-	escolha = input("\n>>> ")
-	lista_escolhida = []
+#!/usr/bin/env python3
 
-	if (escolha == '0' ):
-		port = input("Digite a porta: ")
-		portas_zero = [int(port)] 
-		lista_escolhida = portas_zero
-		return lista_escolhida
-	elif (escolha == '1' ):
-		lista_escolhida = portas_top10
-		return lista_escolhida
-	elif (escolha == '2' ):
-		lista_escolhida = portas_top20
-		return lista_escolhida
-	elif (escolha == '3' ):
-                lista_escolhida = portas_top50
-                return lista_escolhida
-	else:
-		lista_escolhida = portas_total
-		return lista_escolhida
+import sys
 
-def mostraLinha():
-	print("------------------------------------------------------")
+PORTAS = {
+    "1": sorted({21, 22, 23, 25, 53, 80, 110, 139, 443, 445, 3389}),
+    "2": sorted({
+        21, 22, 23, 25, 53, 80, 110, 135, 139, 143,
+        443, 445, 993, 995, 1723, 3306, 3389, 5900, 8080
+    }),
+    "3": sorted({
+        21, 22, 23, 25, 53, 80, 110, 135, 139, 143,
+        443, 445, 993, 995, 1723, 3306, 3389, 5060,
+        5666, 5900, 6001, 8000, 8080, 8443, 8888,
+        10000, 32768, 49152, 49154
+    }),
+    "4": range(1, 65536)
+}
+
+
+def mostrar_banner():
+    print(BANNER)
+    print(AUTHOR)
+    print("-" * 55)
+
+
+def escolher_lista_portas():
+    print("Selecione uma lista de Portas:")
+    print("0 - Uma porta específica")
+    print("1 - Lista Top 10")
+    print("2 - Lista Top 20")
+    print("3 - Lista Top 50")
+    print("4 - Verificar todas as 65535 portas (pode demorar)")
+
+    escolha = input("\n>>> ").strip()
+
+    if escolha == "0":
+        return escolher_porta_unica()
+
+    if escolha in PORTAS:
+        return PORTAS[escolha]
+
+    print("Opção inválida!")
+    sys.exit(1)
+
+
+def escolher_porta_unica():
+    try:
+        porta = int(input("Digite a porta (1-65535): "))
+        if 1 <= porta <= 65535:
+            return [porta]
+        else:
+            raise ValueError
+    except ValueError:
+        print("Porta inválida! Digite um número entre 1 e 65535.")
+        sys.exit(1)
+
+
+def mostrar_linha():
+    print("-" * 55)
+
+
+if __name__ == "__main__":
+    mostrar_banner()
+    portas = escolher_lista_portas()
+    mostrar_linha()
+    print(f"Total de portas selecionadas: {len(portas)}")
+    print(portas)
